7784c57d7002cfa11eda4e57b7bc37430f514c5f
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-04-07 16:02:35 -0400

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x == 0)
+        {
+            return true;
+        }
+        if (x < 0 || x % 10 == 0)
+        {
+            return false;
+        }
+
+
+        int reverse = 0;
+
+        while (x > reverse)
+        {
+            int reminder = x % 10;
+            reverse = (reverse * 10) + reminder;
+            x = x / 10;
+        }
+
+        if (x == reverse || x == reverse / 10)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
