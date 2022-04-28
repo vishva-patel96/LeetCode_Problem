0faacfbda97c141ed099987944405fc3a371ba4d
Vishva Patel <58222962+vishva-patel96@users.noreply.github.com>
2022-04-28 11:22:35 -0400

    used parse method.

---
+++ sum of digits.cs
@@ -0,0 +1,35 @@
+﻿
+//Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Number
+
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine("enter number:");
+            int number = int.Parse(Console.ReadLine());
+            int result = 0, reminder;
+
+
+            while(number > 0)
+            {
+                reminder = number % 10;
+                number = number / 10;
+                result = result+reminder;
+            }
+          
+           Console.WriteLine(result);
+        }
+    }
+}
+
+//input 16 --> 1 + 6 = 7
