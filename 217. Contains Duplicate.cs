221362be12de2bf8f20f8ab3256b5cca574e7017
Golu Patel <1996vishva@gmail.com>
2024-10-24 21:30:49 -0400

    Used Hashset for this problem.

---
+++ 217. Contains Duplicate.cs
@@ -0,0 +1,15 @@
+﻿//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+
+        HashSet<int> hs = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            hs.Add(nums[i]);
+        }
+        return hs.Count != nums.Length;
+    }
+}
